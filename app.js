3c7d14b92ffb066c322dd6117d3505a5a22872ca
MuzammilKhatri9884 <muzzamil.khatri.95@gmail.com>
2025-12-24 16:12:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var Image1 = document.getElementById("Picture");
 function onHover(){
     h1E1.innerText = "Bulb is On"
     Image1.title = "Bulb is On"
-    Image1.src = "./Image/Bulb2.jpg"
+    Image1.src = "./Bulb2.jpg"
 }
 function offHover(){
     h1E1.innerText = "Bulb is Off"
     Image1.title = "Bulb is Off"
-    Image1.src = "./Image/Bulb.jpg"
+    Image1.src = "./Bulb.jpg"
+
 }
